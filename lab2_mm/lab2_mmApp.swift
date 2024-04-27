95d85b4ad49148fa0e66ae023533643262db4b32
Michal Mitura <mitrus98@gmail.com>
2024-04-27 17:15:13 +0200

    Initial Commit

---
+++ lab2_mm/lab2_mmApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab2_mmApp.swift
+//  lab2_mm
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab2_mmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
