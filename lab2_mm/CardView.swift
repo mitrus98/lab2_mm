09c02f624715092629df4ab6b310a8c28e9040bf
Michal Mitura <mitrus98@gmail.com>
2024-04-27 18:43:34 +0200

    test test

---
+++ lab2_mm/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  lab2_mm
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ lab2_mm/card.swift
@@ -0,0 +1,8 @@
+//
+//  card.swift
+//  lab2_mm
+//
+//  Created by student on 27/04/2024.
+//
+
+import Foundation
